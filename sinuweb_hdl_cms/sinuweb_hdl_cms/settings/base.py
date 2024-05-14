65fc4ef015997090b1061919910c483f3a4d5ae7
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-05-15 07:07:11 +1100

    set STATIC_ROOT

--- sinuweb_hdl_cms/sinuweb_hdl_cms/settings/base.py
+++ sinuweb_hdl_cms/sinuweb_hdl_cms/settings/base.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/staticfiles/'
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
