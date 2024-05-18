79681572579dd52d1a53500e67fb85a3ada0715c
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-05-18 14:42:13 +1100

    updated file

--- sinuweb-frontend/lib/fetch-footer.ts
+++ sinuweb-frontend/lib/fetch-footer.ts
@@ -2,6 +2,7 @@ import { cleanData } from '@/utils/utils';
 
 async function fetchFooter(){
 
+
     try {
 
       const response = await fetch(`http://localhost:8000/api/v2/footer/1/?${new URLSearchParams({
@@ -11,13 +12,13 @@ async function fetchFooter(){
       let data = await response.json();
       data = cleanData(data); 
       return data;
-
     } catch (error) {
-      console.error("Error fetching hero:", error);
-      return {}; // Return empty object if there's an error
+      return null;
     }
-    
 
+  
+    
+      
 }
 
 export default fetchFooter;
