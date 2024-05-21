b734e3291a974af847c0250149c0f07208e6c574
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-05-21 16:22:27 +1100

    update file

--- sinuweb-frontend/components/hero/hero-text.tsx
+++ sinuweb-frontend/components/hero/hero-text.tsx
@@ -3,7 +3,7 @@ import Title from "@/ui/title";
 function HeroText(text: any) {
     return ( 
         <Title>
-           {text.text}testing continuous integration #2
+           {text.text}welcome to SINU
         </Title>
      );
 }
