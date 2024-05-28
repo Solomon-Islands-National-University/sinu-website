b4253a11f0b102958ca59275dd4ad56522484f34
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-05-29 10:11:31 +1100

    add graphQL

--- sinuweb_hdl_cms/sinuweb_hdl_cms/urls.py
+++ sinuweb_hdl_cms/sinuweb_hdl_cms/urls.py
@@ -9,6 +9,10 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 
 from .api import api_router
+from django.urls import re_path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from api.schema import schema
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -27,13 +31,16 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
+    
     path('api/v2/', api_router.urls),
-
+    path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("", include(wagtail_urls)),
+    # path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
+    path("", include(wagtail_urls)),
+    
+    
 ]
